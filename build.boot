553dab8416aede51e60b1d839c61d0550380c46c
Chao Chen <chao.chen@shopeemobile.com>
2017-12-12 19:16:43 +0800

    create base, not finish

--- build.boot
+++ build.boot
@@ -3,5 +3,35 @@
 (set-env!
   :resource-paths #{"src"}
   :dependencies   '[[org.clojure/clojure "1.8.0" :scope "provided"]
-                    [org.webjars/materializecss "0.100.2"]])
+                    [org.webjars/materializecss "0.100.2" :scope "test"]
+                    [adzerk/bootlaces "0.1.13" :scope "test"]])
+
+(require '[rydb2.boot-webjar-sources :refer [webjar-sources]]
+         '[adzerk.bootlaces :refer :all])
+
+(def +version+ "0.0.1")
+
+(task-options!
+  webjar-sources {:name "materializecss"
+                  :matching ".*/fonts/.*.(woff|woff2|ttf)$"
+                  :target "target/fonts"}
+  pom {:project     'rydb2/boot-webjar-sources
+       :version     +version+
+       :description "Boot task to copy sources in webjar (like fonts)"
+       :url         "https://github.com/rydb2/boot-webjar-sources"
+       :scm         {:url "https://github.com/deraen/boot-less"}
+       :license     {"Eclipse Public License" "http://www.eclipse.org/legal/epl-v10.html"}})
+
+
+(deftask dev []
+  (comp (webjar-sources)
+        (target :dir #{"target"})))
+
+(bootlaces! +version+)
+
+(deftask build []
+  (comp
+   (pom)
+   (jar)
+   (install)))
 
--- src/rydb2/boot_webjar_sources.clj
+++ src/rydb2/boot_webjar_sources.clj
@@ -1,27 +1,67 @@
 (ns rydb2.boot-webjar-sources
-  {:boot/export-tasks true}
+  ;; {:boot/export-tasks true}
   (:require
    [clojure.java.io :as io]
    [boot.pod        :as pod]
    [boot.core       :as core]
    [boot.util       :as util]
-   [clojure.sting   :as str])
+   [clojure.string   :as str])
   (:import [java.util.jar JarFile JarEntry]))
 
 (def ^:private webjars-pattern
-  #"META-INF/resources/webjars/([^/]+)/([^/]+)/(.*)")
+  #"META-INF/resources/webjars/([^/]+)/([^/]+)/(.*)$")
 
-(defn ^:private asset-path [resource]
+(defn asset-path [resource]
   (let [[_ name version path] (re-matches webjars-pattern resource)]
     (str name "/" path)))
 
-(defn ^:private webjars-path-regex [name]
+(defn webjars-path-regex [name]
   (let [re-str (format ".*/webjars/%s/.*/%s-.*\\.jar$" name name)]
     (re-pattern re-str)))
 
 (defn asset-webjar [name paths]
   (filter #(re-matches (webjars-path-regex name) %) paths))
 
-(defn get-sources [name]
-  (if-let [jar (first (asset-webjar name (str/split (:fake-class-path pod/env) ":")))]
-    (enumeration-seq (.entries jar))))
+(defn get-file-name [path]
+  (last (str/split path #"/")))
+
+
+(defn get-jar-file [name]
+  (if-let [jar-path (first (asset-webjar name (str/split (:fake-class-path pod/env) #":")))]
+    (JarFile. jar-path)))
+
+(defn get-entries [name matching]
+  (if-let [jar-file (get-jar-file name)]
+    (->> jar-file
+         (.entries)
+         (enumeration-seq)
+         (filter (defn f [entry]
+                   (let [name (.getName ^JarEntry entry)]
+                     (and
+                       (re-matches webjars-pattern name)
+                       (re-matches (re-pattern matching) name))))))
+    (print "error")))
+
+(core/deftask webjar-sources
+  "copy webjar sources"
+  [n name str "webjar name"
+   m matching str "sources path regex string in webjar"
+   t target str "output dir"]
+  (let [entries (get-entries name matching)
+        jar-file (get-jar-file name)]
+    (core/with-pre-wrap fileset
+      (doseq [entry entries]
+        (let [jar-name (.getName ^JarEntry entry)
+              in-file (.getInputStream jar-file entry)
+              out-file (doto
+                           (io/file target (get-file-name jar-name))
+                           io/make-parents)
+              tmp-in-files (core/input-files fileset)]
+          (do
+            (util/info "%s\ncopy %s \nto   %s\n" name (.getName ^JarEntry entry) out-file)
+            (io/make-parents (.getParent out-file))
+            (io/copy in-file out-file))))
+     (-> fileset
+         (core/add-resource (io/file (.getParent (io/file target))))
+         core/commit!))))
+
